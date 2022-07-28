bc6b7f65b373a3e05cc1c030f49bd80317213a7d
Tonio Caputo <antonio.caputo@certaconsulting.net>
2022-07-28 15:12:52 -0300

    javadoc in Resource

--- src/main/java/org/acme/getting/started/ByeResource.java
+++ src/main/java/org/acme/getting/started/ByeResource.java
@@ -5,9 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * The Class ByeResource.
+ */
 @Path("/bye")
 public class ByeResource {
 
+
+    /**
+     * Hello.
+     *
+     * @return the string
+     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
